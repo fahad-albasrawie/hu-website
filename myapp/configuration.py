83c2884f370c6ee3abb905cdfdb134f6428fbccd
unknown <cabbaascadde55@gmail.com>
2025-07-07 11:55:57 +0300

    DB Configuration update

--- myapp/configuration.py
+++ myapp/configuration.py
@@ -2,22 +2,18 @@ import os
 
 
 class AdminDbConfiguration:
-    DB_HOSTNAME = '185.7.81.97'
+    DB_HOSTNAME = '104.237.2.47'
     DB_USERNAME = 'fahad'
-    DB_NAME = 'albasrawie'
+    DB_NAME = 'hu_article'
     DB_PASSWORD = 'Assaamarraaii!@12'
-    EMAIL_PASS = 'ukcbzrabhzzmvhcz'
-    FROM_EMAIL = 'albasrawie.cs.edu@gmail.com'
-    TO_EMAIL = 'cabbaascadde55@gmail.com'
+
 
 class ArticleDbConfiguration:
-    DB_HOSTNAME = '185.7.81.97'
+    DB_HOSTNAME = '104.237.2.47'
     DB_USERNAME = 'fahad'
-    DB_NAME = 'maqaal'
+    DB_NAME = 'hu_article'
     DB_PASSWORD = 'Assaamarraaii!@12'
-    EMAIL_PASS = 'ukcbzrabhzzmvhcz'
-    FROM_EMAIL = 'albasrawie.cs.edu@gmail.com'
-    TO_EMAIL = 'cabbaascadde55@gmail.com'
+
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or os.urandom(24)
