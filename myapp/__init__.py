fd52ba771999b28ebc2b1f0359abbeae548d1118
unknown <cabbaascadde55@gmail.com>
2025-07-08 12:13:02 +0300

    Waxaanu ka shaqeeyey beddelka sawirrada graduations

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # CORS(app, resources={r"/api/*": {"origins": "*"}})  # Enable CORS for all routes
 
-from myapp.public import public_model, public_views
+from myapp.public import public_model, public_views, quick_data
 from myapp.article import article_views, article_model
---
+++ myapp/public/quick_data.py
@@ -0,0 +1,163 @@
+graduators_words = {
+    'number1': {
+        'name': 'Ahmaed Khalif Muumin',
+        'faculty': 'Faculty of Computer Science',
+        'class': 'Class of 2023',
+        'image': 'static/img/Ahmed_Khalif_Mumin.png',  # Corrected filename to match HTML
+        'word': "Joining Hormuud University's Computer Science program was a gateway to a world of endless possibilities. From my very first semester, I was immersed in a challenging yet supportive environment that fostered innovation, critical thinking, and problem-solving."
+    },
+    'number2': {
+        'name': 'Mushtak Omar Hussein',
+        'faculty': 'Faculty of Business Administration',
+        'class': 'Class of 2023',
+        'image': 'static/img/Mushtak_Omar_Hussein.png',
+        'word': "My journey at Hormuud University's Business Administration faculty has been nothing short of transformative. When I first stepped onto campus, I had ambitions of becoming a business leader but wasn't sure how to chart the course. Hormuud University provided me with the compass and the map."
+    },
+    'number3': {
+        'name': 'Ikram Abdi Osman',
+        'faculty': 'Faculty of Information Technology',
+        'class': 'Class of 2023',
+        'image': 'static/img/Ikram_Abdi_Osman.png',
+        'word': "Choosing Hormuud University for my IT education was a pivotal decision in my life. It provided me with more than just a degree; it was a comprehensive experience that shaped my understanding of technology and its impact on the world."
+    },
+    'number4': {
+        'name': 'Abdikarin Mohamed Ali (Daanduri)',
+        'faculty': 'Faculty of Computer Science',
+        'class': 'Class of 2023',
+        'image': 'static/img/Abdikarin_Mohamed_Ali.png',
+        'word': "Embarking on my Computer Science degree at Hormuud University has been one of the most rewarding endeavors of my life. With a curriculum that is as challenging as it is inspiring, I was able to dive deep into the complexities of computing and emerge with a profound understanding of its principles and applications."
+    },
+    'number5': {
+        'name': 'Abdiwali Farah Abdi',
+        'faculty': 'Faculty of Electrical Engineering',
+        'class': 'Class of 2023',
+        'image': 'static/img/Abdiwali_Farah_Abdi.png',
+        'word': "From the moment I began my journey at Hormuud University in the Electrical Engineering faculty, I knew that I was on a path to discovery and innovation. The university's vibrant academic environment and cutting-edge resources provided the perfect platform for nurturing my passion for engineering."
+    },
+    'number6': {
+        'name': 'Nasteho Ali Hassan',
+        'faculty': 'Faculty of Electrical Engineering',
+        'class': 'Class of 2023',
+        'image': 'static/img/Nasteho_Ali_Hassan.png',
+        'word': "As I reflect on my time at Hormuud University, I am filled with a profound sense of accomplishment and gratitude. The Electrical Engineering program here has been a crucible for innovation, critical thinking, and technical excellence, all of which have been pivotal in my educational journey."
+    },
+    'number7': {
+        'name': 'Mohamed Abdullahi Mohamud',
+        'faculty': 'Faculty of Electrical Engineering',
+        'class': 'Class of 2023',
+        'image': 'static/img/Mohamed_Abdullahi_Mohamud.png',
+        'word': "Enrolling in the Electrical Engineering program at Hormuud University was the first step towards realizing my dream of becoming an engineer capable of significant technological contributions. The program's robust curriculum, which encompasses the vast field of electrical systems provided a comprehensive educational experience."
+    },
+    'number8': {
+        'name': 'Abdikafi Omar Abdi',
+        'faculty': 'Faculty of Economics',
+        'class': 'Class of 2023',
+        'image': 'static/img/Abdikafi_Omar_Abdi.png',
+        'word': "My academic voyage at Hormuud University in the Economics program has been a journey of growth, insight, and enlightenment. The university's robust curriculum provided a comprehensive exploration of economic theories and their real-world applications, equipping me with a deep understanding of complex economic systems."
+    },
+    'number9': {
+        'name': 'Hani Yusuf Abdi',
+        'faculty': 'Faculty of Economics',
+        'class': 'Class of 2023',
+        'image': 'static/img/Hani_Yusuf_Abdi.png',
+        'word': "Embarking on my academic journey in the Economics program at Hormuud University has been a transformative and enlightening experience. The program offered a perfect amalgamation of theoretical knowledge and practical insights, allowing me to delve deeply into the complexities of global and local economies."
+    },
+    'number10': {
+        'name': 'Abdirahman Esse Salad',
+        'faculty': 'Faculty of Geology',
+        'class': 'Class of 2023',
+        'image': 'static/img/Abdirahman_Esse_Salad.png',
+        'word': "My journey as a Geology student at Hormuud University has been nothing short of extraordinary. From the very first day, I was immersed in an environment that not only values scientific exploration but also encourages a deeper connection with our planet."
+    },
+    'number11': {
+        'name': 'Raida Ali Mohamud',
+        'faculty': 'Faculty of Geology',
+        'class': 'Class of 2023',
+        'image': 'static/img/Raida_Ali_Mohamud.png',
+        'word': "Studying Geology at Hormuud University has been an adventure of discovery and enlightenment. The program’s depth and breadth exposed me to the fascinating world beneath our feet, from the ancient processes that shape the earth to the modern techniques used in studying them."
+    },
+    'number12': {
+        'name': 'Aadan Naaleye Ali',
+        'faculty': 'Faculty of Islamic Banking and Finance',
+        'class': 'Class of 2023',
+        'image': 'static/img/Adan_Naleye_Ali.png',  # Corrected filename (Adan_Naleye_Ali.png)
+        'word': "Choosing to pursue my studies in Islamic Banking and Finance at Hormuud University has been one of the most fulfilling decisions of my academic career. The program offered a unique blend of finance principles grounded in Islamic ethical values, providing me with a holistic understanding of the financial world."
+    },
+    'number13': {
+        'name': 'Leyla Said Ahmed',
+        'faculty': 'Faculty of Islamic Banking and Finance',
+        'class': 'Class of 2023',
+        'image': 'static/img/Leyla_Said_Ahmed.png',
+        'word': "My academic journey at Hormuud University in the Islamic Banking and Finance program has been both enlightening and transformative. The program's unique approach to finance, rooted in Islamic principles, provided me with a fresh perspective on the financial world and its ethical implications."
+    },
+    'number14': {
+        'name': 'Aisha Abdukadir Ahmed',
+        'faculty': 'Faculty of Public Relationship',
+        'class': 'Class of 2023',
+        'image': 'static/img/Aisha_Abdukadir_Ahmed.png',
+        'word': "Studying Public Relations at Hormuud University has been a transformative experience. The program offered a dynamic blend of theory and practice, equipping me with the tools to navigate the fast-paced world of media and communications."
+    },
+    'number15': {
+        'name': 'Mohamed Musse Abdulle',
+        'faculty': 'Faculty of Public Relationship',
+        'class': 'Class of 2023',
+        'image': 'static/img/Mohamed_Musse_Abdulle.png',
+        'word': "My journey in the Public Relations program at Hormuud University has been nothing short of extraordinary. The program's curriculum, rich in both theoretical knowledge and practical application, has thoroughly prepared me for a career in PR."
+    },
+    'number16': {
+        'name': 'Hussein Ali Mohamed',
+        'faculty': 'Faculty of Sharia and Leadership',
+        'class': 'Class of 2023',
+        'image': 'static/img/Hussein_Ali_Mohamed.png',
+        'word': "My educational journey in the Sharia and Leadership program at Hormuud University has been profoundly enriching and enlightening. The program seamlessly blended the timeless wisdom of Sharia law with modern leadership principles, preparing me for a role that demands both ethical integrity and effective leadership skills."
+    },
+    'number17': {
+        'name': 'Abdullahi Abdinasir Jama',
+        'faculty': 'Faculty of Sharia and Leadership',
+        'class': 'Class of 2023',
+        'image': 'static/img/Abdullahi_Abdinasir_Jama.png',
+        'word': "Enrolling in the Sharia and Leadership program at Hormuud University marked the beginning of a transformative chapter in my life. The program offered a unique combination of in-depth Sharia studies and practical leadership training, providing me with a comprehensive understanding of Islamic ethical principles and how they guide effective leadership."
+    },
+    'number18': {
+        'name': 'Khalid Mohamed Osman',
+        'faculty': 'Faculty of Sharia and Leadership',
+        'class': 'Class of 2023',
+        'image': 'static/img/Khalid_Mohamed_Osman.png',
+        'word': "The Sharia and Leadership program at Hormuud University has been a remarkable journey of personal and professional growth. This program bridged the gap between the rich traditions of Islamic Sharia and the contemporary practices of effective leadership."
+    },
+    'number19': {
+        'name': 'Abdirahin Osman Abtidon',
+        'faculty': 'Faculty of Telecommunications',
+        'class': 'Class of 2023',
+        'image': 'static/img/Abdirahin_Osman_Abtidon.png',
+        'word': "Enrolling in the Telecommunications program at Hormuud University was a decision that set the stage for my future in the ever-evolving world of technology. The program’s rigorous curriculum covered a wide array of topics, from network infrastructure and wireless communications to digital signal processing."
+    },
+    'number20': {
+        'name': 'Hibo Ali Mohamed',
+        'faculty': 'Faculty of Telecommunications',
+        'class': 'Class of 2023',
+        'image': 'static/img/Hibo_Ali_Mohamed.png',
+        'word': "My experience in the Telecommunications program at Hormuud University has been nothing short of transformative. The blend of theoretical learning with practical application allowed me to deeply understand and engage with the principles of telecommunications."
+    },
+    'number21': {
+        'name': 'Bisharo Abdiaziz Ibrahim',
+        'faculty': 'Faculty of Telecommunications',
+        'class': 'Class of 2023',
+        'image': 'static/img/Bisharo_Abdiaziz_Ibrahim.png',
+        'word': "Choosing Hormuud University for my studies in Telecommunications has been a pivotal point in my educational journey. The program’s comprehensive curriculum, encompassing both the technical and managerial aspects of telecommunications, provided a balanced and thorough education."
+    },
+    'number22': {
+        'name': 'Sumaya Said Barre',
+        'faculty': 'Faculty of Logistics',
+        'class': 'Class of 2023',
+        'image': 'static/img/Sumaya_Said_Barre.png',
+        'word': "Enrolling in the Logistics program at Hormuud University was a turning point in my academic and professional life. The program offered a perfect blend of theoretical knowledge and practical skills essential for a career in logistics and supply chain management."
+    },
+    'number23': {
+        'name': 'Ahmed Mohamed Abdibari',
+        'faculty': 'Faculty of Logistics',
+        'class': 'Class of 2023',
+        'image': 'static/img/Ahmed_Mohamed_Abdibari.png',
+        'word': "My educational journey in the Logistics program at Hormuud University has been both challenging and rewarding. The curriculum, designed to cover all aspects of logistics and supply chain management, provided me with a comprehensive understanding of the field."
+    }
+}
